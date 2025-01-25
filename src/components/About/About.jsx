4bc4ee24a5129605860bae74ff55b2e11a61249d
Suyash Mullick <sm224cb@student.lnu.se>
2025-01-25 20:05:11 +0100

    Initial commit - adding website content

---
+++ src/components/About/About.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+
+import styles from "./About.module.css";
+import { getImageUrl } from "../../utils";
+
+export const About = () => {
+  return (
+    <section className={styles.container} id="about">
+      <h2 className={styles.title}>About</h2>
+      <div className={styles.content}>
+        <img
+          src={getImageUrl("about/laptop.png")}
+          alt="Me sitting with a laptop"
+          className={styles.aboutImage}
+        />
+        <ul className={styles.aboutItems}>
+          <li className={styles.aboutItem}>
+            <img src={getImageUrl("about/cursorIcon.png")} alt="Cursor icon" />
+            <div className={styles.aboutItemText}>
+              <h3>Aspiring ML Engineer</h3>
+              <p>
+                --------
+              </p>
+            </div>
+          </li>
+          <li className={styles.aboutItem}>
+            <img src={getImageUrl("about/serverIcon.png")} alt="Server icon" />
+            <div className={styles.aboutItemText}>
+              <h3>Java Developer</h3>
+              <p>
+                I have experience on projects working back-end using Java and MySQL.
+              </p>
+            </div>
+          </li>
+          <li className={styles.aboutItem}>
+            <img src={getImageUrl("about/cursorIcon.png")} alt="UI icon" />
+            <div className={styles.aboutItemText}>
+              <h3>Web Developer</h3>
+              <p>
+                I have designed and deployed multiple webpages with knowledge in HTML, CSS and JavaScript.
+                I also have worked with React and Streamlit, and am learning React Native with the aim to build
+                a mobile app.
+              </p>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </section>
+  )
+}
---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+import styles from "./Contact.module.css";
+import { getImageUrl } from "../../utils";
+
+export const Contact = () => {
+  return (
+    <footer id="contact" className={styles.container}>
+      <div className={styles.text}>
+        <h2>Contact</h2>
+        <p>Feel free to reach out!</p>
+      </div>
+      <ul className={styles.links}>
+        <li className={styles.link}>
+          <img src={getImageUrl("contact/emailIcon.png")} alt="Email icon" />
+          <a href="mailto:suyash.mullick@gmail.com">suyash.mullick@gmail.com</a>
+        </li>
+        <li className={styles.link}>
+          <img
+            src={getImageUrl("contact/linkedinIcon.png")}
+            alt="LinkedIn icon"
+          />
+          <a href="https://www.linkedin.com/in/suyash-mullick-0844b1271/">LinkedIn</a>
+        </li>
+        <li className={styles.link}>
+          <img src={getImageUrl("contact/githubIcon.png")} alt="Github icon" />
+          <a href="https://www.github.com/SuyashMullick">GitHub</a>
+        </li>
+      </ul>
+    </footer>
+  );
+};
