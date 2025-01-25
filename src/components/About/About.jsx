6501c6d2cfc32c843bd6b92386dcda3aff554394
Suyash Mullick <sm224cb@student.lnu.se>
2025-01-25 21:50:54 +0100

    Some fixes

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -15,11 +15,14 @@ export const About = () => {
         />
         <ul className={styles.aboutItems}>
           <li className={styles.aboutItem}>
-            <img src={getImageUrl("about/cursorIcon.png")} alt="Cursor icon" />
+            <img src={getImageUrl("about/mlIcon.png")} alt="ML icon" />
             <div className={styles.aboutItemText}>
               <h3>Aspiring ML Engineer</h3>
               <p>
-                --------
+                I have been learning ML through online courses and hands-on projects on topics
+                such as Linear Regression on PyTorch. I want to continue building my skills in ML
+                by working on real-world projects, exploring advanced techniques like deep learning,
+                and applying AI to solve practical problems.
               </p>
             </div>
           </li>
@@ -28,7 +31,9 @@ export const About = () => {
             <div className={styles.aboutItemText}>
               <h3>Java Developer</h3>
               <p>
-                I have experience on projects working back-end using Java and MySQL.
+                I have experience on projects working back-end using Java and MySQL. Throughout
+                my studies, I have also gained knowedge & experience on various software development
+                methodologies.
               </p>
             </div>
           </li>
@@ -38,7 +43,7 @@ export const About = () => {
               <h3>Web Developer</h3>
               <p>
                 I have designed and deployed multiple webpages with knowledge in HTML, CSS and JavaScript.
-                I also have worked with React and Streamlit, and am learning React Native with the aim to build
+                I also have worked with React and Streamlit, and I am learning React Native with the aim to build
                 a mobile app.
               </p>
             </div>
